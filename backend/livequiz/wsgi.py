4f601eb328b6a235e60315c52b2aebf6a6871307
varunkasnia <varunkasnia73@gmail.com>
2026-02-17 21:50:34 +0530

    Delete backend directory

--- backend/livequiz/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI config for livequiz project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livequiz.settings')
-
-application = get_wsgi_application()
--- backend/quiz/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/quiz/(?P<quiz_id>[0-9a-f-]+)/$', consumers.QuizConsumer.as_asgi()),
-]
